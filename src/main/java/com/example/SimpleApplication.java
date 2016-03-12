3a512b28524051e473a064de5cca452abaa20cdd
Bas W. Knopper <basknopper@gmail.com>
2016-03-12 17:42:31 +0100

    Implements first actual call to fitbit when logged in. Not using the data yet though.

--- src/main/java/com/example/SimpleApplication.java
+++ src/main/java/com/example/SimpleApplication.java
@@ -1,10 +1,13 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +18,9 @@ import java.security.Principal;
 @EnableOAuth2Sso
 @RestController
 public class SimpleApplication extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private OAuth2RestOperations oauth2RestTemplate;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -30,6 +36,18 @@ public class SimpleApplication extends WebSecurityConfigurerAdapter {
 	@RequestMapping("/user")
 	public Principal user(Principal principal) {
 		return principal;
+	}
+
+
+	@RequestMapping("/chr")
+	public String chr() {
+
+		ResponseEntity<String> response = oauth2RestTemplate.getForEntity(
+				"https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1sec/time/00:00/23:59.json",
+				String.class);
+
+		System.out.println(response);
+		return response.toString();
 	}
 
 	public static void main(String[] args) {
